0e87bfe6d1776a64f5a582ce9c15fcff3e5588b6
Adam Aaron <adam.aaron@tribalogic.net>
2019-01-06 15:03:00 +0000

    Add site folder

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,21 @@
+var store = [{
+        "title": "Forward",
+        "excerpt":"How would I define ‘digital watercolour’ for the purposes of this course? Digital watercolour refers to an electronic representation of the fundamental characteristics of analogue watercolour. Let’s break that down a bit more: Electronic representation equates to a grid of pixels like any other digital raster image such as a...","categories": [],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/digital_watercolour_course/forward/",
+        "teaser":null},{
+        "title": "Hello world",
+        "excerpt":"I’m not 100% sure what this little corner of the internet will be for just yet. I started this site out of frustration for feeling like I want to share something but I am not sure what. We will see… ","categories": ["site"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/site/hello-world/",
+        "teaser":null},{
+        "title": "The idea of a digital sketch collection",
+        "excerpt":"The likes of Instagram, Facebook and Twitter et al leave me cold and numb. Dear Instagram, Good Bye and Good Riddance. In comparison to an analogue sketchbook there really is no real digital equivalent to me. We live in an age of mass consumption. But are we really consuming anything?...","categories": ["opinion"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/opinion/digital-sketch-collection/",
+        "teaser":"http://0.0.0.0:4000/assets/images/2017-11-12-national-musuem-collection.jpg"},{
+        "title": "Introduction",
+        "excerpt":"The sum of many parts To get a realistic representation of analogue watercolour takes many small ingredients that work together in unison. We also want to build a method of working that keeps out of our way as much as possible. I have separated my workflow into five main stages,...","categories": ["Courses"],
+        "tags": ["Procreate","watercolour","workflow"],
+        "url": "http://0.0.0.0:4000/courses/Sum-of-many-parts/",
+        "teaser":null},]
